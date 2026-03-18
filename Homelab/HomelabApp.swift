d853b551abd43484438f163e66bef6a7fcda6001
Mathieu Dubart <mathieu.dubart@icloud.com>
2026-03-18 19:38:04 +0100

    Initial Commit

---
+++ Homelab/HomelabApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomelabApp.swift
+//  Homelab
+//
+//  Created by Mathieu Dubart on 18/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct HomelabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
